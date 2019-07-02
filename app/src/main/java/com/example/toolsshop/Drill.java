7c7123ad018039a335c948ae14e3696f15d9e262
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2019-07-02 20:41:00 +0200

    create DrillDetailActivity

---
+++ app/src/main/java/com/example/toolsshop/Drill.java
@@ -0,0 +1,31 @@
+package com.example.toolsshop;
+
+public class Drill {
+
+    private String title;
+    private String info;
+    private int imgResID;
+
+    public Drill(String title, String info, int imgResID) {
+        this.title = title;
+        this.info = info;
+        this.imgResID = imgResID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public int getImgResID() {
+        return imgResID;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
--- app/src/main/java/com/example/toolsshop/DrillCategoryActivity.java
+++ app/src/main/java/com/example/toolsshop/DrillCategoryActivity.java
@@ -3,7 +3,10 @@ package com.example.toolsshop;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DrillCategoryActivity extends AppCompatActivity {
 
@@ -20,5 +23,13 @@ public class DrillCategoryActivity extends AppCompatActivity {
         }
 
         listViewDrills = findViewById(R.id.listViewDrills);
+
+        ArrayList<Drill> drills = new ArrayList<>();
+        drills.add(new Drill(getString(R.string.drill_dewalt_title), getString(R.string.drill_dewalt_info), R.drawable.dewalt));
+        drills.add(new Drill(getString(R.string.drill_makita_title), getString(R.string.drill_makita_info), R.drawable.makita));
+        drills.add(new Drill(getString(R.string.drill_bosch_title), getString(R.string.drill_bosch_info), R.drawable.bosch));
+
+        ArrayAdapter<Drill> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, drills);
+        listViewDrills.setAdapter(adapter);
     }
 }

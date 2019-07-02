74a17ce3af751da3276c0378b765ee9b577ffdc7
Pavel Samsonenka <pavel.samsonenka92@gmail.com>
2019-07-02 19:59:03 +0200

    class Deill

--- app/src/main/java/com/example/toolsshop/DrillCategoryActivity.java
+++ app/src/main/java/com/example/toolsshop/DrillCategoryActivity.java
@@ -1,13 +1,24 @@
 package com.example.toolsshop;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class DrillCategoryActivity extends AppCompatActivity {
+
+    private ListView listViewDrills;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drill_category);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+
+        listViewDrills = findViewById(R.id.listViewDrills);
     }
 }
